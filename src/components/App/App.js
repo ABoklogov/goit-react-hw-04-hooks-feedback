91feee86212782f7bd811f81a9b350e9de203e18
Alexey Boklogov <kashira87@mail.ru>
2021-08-18 19:29:09 +0300

    убрал лишний код

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Statistics from '../Statistics';
 import FeedbackOptions from '../FeedbackOptions';
 import Section from '../Section';
@@ -7,11 +7,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
+
   const arrState = ['good', 'neutral', 'bad'];
 
   const addReview = review => {
@@ -33,14 +29,6 @@ const App = () => {
     }
   };
 
-  // let totalFeedback = 0;
-  // let positiveFeedbackPercentage = 0;
-
-  // useEffect(() => {
-  //   totalFeedback = 1;
-  //   positiveFeedbackPercentage = Math.round((good * 100) / totalFeedback);
-  // }, [good, neutral, bad]);
-
   const countTotalFeedback = () => {
     return good + neutral + bad;
   };
@@ -49,16 +37,6 @@ const App = () => {
     const total = countTotalFeedback();
     return Math.round((good * 100) / total);
   };
-
-  // this.setState(prevReview => ({
-  //   [review]: prevReview[review] + 1,
-  // }));
-
-  // render() {
-  // const { good, neutral, bad } = this.state;
-  // const arrState = Object.keys(this.state);
-  // const totalFeedback = this.countTotalFeedback();
-  // const positivePercentage = this.countPositiveFeedbackPercentage();
 
   return (
     <>
@@ -74,7 +52,6 @@ const App = () => {
       </Section>
     </>
   );
-  // }
 };
 
 export default App;
